1503641df1ba7a614a791b8114d17149cd17bf79
matheusktt <matheus_0119@outlook.com>
2023-03-18 21:33:12 -0300

    annotation repository

--- src/main/java/com/devsuperior/bds03/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/bds03/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.devsuperior.bds03.repositories;
 
 import com.devsuperior.bds03.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
